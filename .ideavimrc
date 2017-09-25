3ce6d2da2fd18285cd52996cda924ab44455473e
Ryan Faulhaber <faulhaber.14@osu.edu>
2017-09-25 07:51:38 -0400

    updated script, added .ideavimrc

---
+++ .ideavimrc
@@ -0,0 +1,3 @@
+set relativenumber
+set incsearch
+set hlsearch
--- link.sh
+++ link.sh
@@ -1,21 +1,28 @@
 #!/usr/bin/env bash
 
 if [ -f $HOME/.Xresources ]; then
-	ln $HOME/.Xresources $HOME/Projects/dotfiles
+	ln $HOME/.Xresources .
     echo "Linked .Xresources file"
 else
 	echo "No .Xresources found"
 fi
 
 if [ -f $HOME/.vimrc ]; then
-	ln $HOME/.vimrc $HOME/Projects/dotfiles
+	ln $HOME/.vimrc .
     echo "linked .vimrc"
 else
 	echo "No .vimrc found"
 fi
 
+if [ -f $HOME/.ideavimrc ]; then
+	ln $HOME/.ideavimrc .
+    echo "linked .ideavimrc"
+else
+	echo "No .ideavimrc found"
+fi
+
 if [ -f $HOME/.config/i3/config ]; then
-	ln $HOME/.config/i3/config $HOME/Projects/dotfiles
+	ln $HOME/.config/i3/config .
     echo "linked i3"
 else
 	echo "No i3 config file found"
@@ -23,14 +30,14 @@ fi
 
 if [[ $OSTYPE == "darwin"* ]]; then
     if [ -d $HOME/Library/Application\ Support/Code ]; then
-        ln $HOME/Library/Application\ Support/Code/User/settings.json $HOME/Projects/dotfiles
+        ln $HOME/Library/Application\ Support/Code/User/settings.json .
         echo "linked vscode for mac"
     else 
         echo "No vscode settings found on macOS"
     fi
 elif [ $OSTYPE == "linux-gnu" ]; then
     if [ -d $HOME/.config/Code ]; then
-        ln $HOME/.config/Code/User/settings.json $HOME/Projects/dotfiles
+        ln $HOME/.config/Code/User/settings.json .
         echo "linked vscode for linux"
     else
         echo "No vscode settings found on Linux"
