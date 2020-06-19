3a358f94dc83b4fae6404c8d9bb7e927221b7809
Ryan Faulhaber <faulhaberryan@gmail.com>
2020-06-19 09:52:59 -0400

    friendship ended with vterm, eshell is my new best friend. also ivy

--- doom.d/init.el
+++ doom.d/init.el
@@ -20,8 +20,8 @@
 
        :completion
        company           ; the ultimate code completion backend
-       ;;helm              ; the *other* search engine for love and life
-       ;;ido               ; the other *other* search engine...
+       ;; helm              ; the *other* search engine for love and life
+       ;; ido               ; the other *other* search engine...
        ivy               ; a search engine for love and life
 
        :ui
@@ -71,9 +71,9 @@
 
        :term
        eshell            ; the elisp shell that works everywhere
-       shell             ; simple shell REPL for Emacs
+       ;;shell             ; simple shell REPL for Emacs
        ;;term              ; basic terminal emulator for Emacs
-       vterm             ; the best terminal emulation in Emacs
+       ;;vterm             ; the best terminal emulation in Emacs
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
