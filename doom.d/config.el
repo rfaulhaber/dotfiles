80a859db3269e619929539f21ac91bc1c6eb2419
Ryan Faulhaber <ryf@sent.as>
2020-12-05 15:20:57 -0500

    [DOOM] increase font-size for 4k display on nix

--- doom.d/config.el
+++ doom.d/config.el
@@ -23,7 +23,9 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "Hack Nerd Font" :size 14))
+
+(setq self/font-size (if (string= (system-name) "mir3") 24 14))
+(setq doom-font (font-spec :family "Hack Nerd Font" :size self/font-size))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
