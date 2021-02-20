cbbc05d5985ca2abdecfce463a28113634f79cd0
Ryan Faulhaber <ryf@sent.as>
2021-02-20 15:08:59 -0500

    [NIX] change random wallpaper query

--- nix/hosts/mir3/random-wallpaper.sh
+++ nix/hosts/mir3/random-wallpaper.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 KEY=$(pass wallpaper)
-API="https://api.unsplash.com/photos/random/?client_id=$KEY&orientation=landscape&query=desktop-background"
+API="https://api.unsplash.com/photos/random/?client_id=$KEY&orientation=landscape&query=desktop-wallpapers"
 
 dir=$(mktemp -d)
 
