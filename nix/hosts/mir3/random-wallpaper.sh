7b307140e5277cdad7eb8a72fcc23483d26ed5f0
Ryan Faulhaber <ryf@sent.as>
2021-04-28 09:53:49 -0400

    [MISC] updated random wallpaper script for logging

--- nix/hosts/mir3/random-wallpaper.sh
+++ nix/hosts/mir3/random-wallpaper.sh
@@ -3,14 +3,23 @@ set -euo pipefail
 
 KEY=$(pass wallpaper)
 API="https://api.unsplash.com/photos/random/?client_id=$KEY&orientation=landscape&query=desktop-wallpapers"
+LOG_FILE=~/.wallpaper-log.txt
 
 dir=$(mktemp -d)
 
-req=$(curl $API | jq '.urls.full')
+req=$(curl $API)
+id=$(echo $req | jq '.id' | tr -d '"')
+img_url=$(echo $req | jq '.urls.full')
+desc=$(echo $req | jq '.description // .alt_description')
 
-for i in $req
-do
-    curl -o wallpaper --output-dir $dir $(echo $i | tr -d '"')
-done
+url=$(echo $img_url | tr -d '"')
+timestamp=$(date '+%Y/%m/%d %H:%M:%S')
+filename="$dir/$id.jpg"
 
-feh --bg-fill $dir/wallpaper
+curl -o $filename $url
+
+tmp_log=$(mktemp)
+jq --arg id "$id" --arg description "$desc" --arg timestamp "$timestamp" --arg url "$url" '. += [{id: $id, description: $description, timestamp: $timestamp, url: $url}]' $LOG_FILE > $tmp_log
+mv $tmp_log $LOG_FILE
+
+feh --bg-fill $filename
