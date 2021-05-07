c9801096d32006cac5f0bb99930356e4fc81b156
Ryan Faulhaber <ryf@sent.as>
2021-05-07 09:37:02 -0400

    [SELF] random wallpaper script updated

--- nix/hosts/mir3/random-wallpaper.sh
+++ nix/hosts/mir3/random-wallpaper.sh
@@ -8,18 +8,24 @@ LOG_FILE=~/.wallpaper-log.txt
 dir=$(mktemp -d)
 
 req=$(curl $API)
-id=$(echo $req | jq '.id' | tr -d '"')
-img_url=$(echo $req | jq '.urls.full')
-desc=$(echo $req | jq '.description // .alt_description')
+id=$(echo $req | jq -r '.id')
+img_url=$(echo $req | jq -r '.urls.full')
+desc=$(echo $req | jq -r '.description // .alt_description')
+html_link=$(echo $req | jq -r '.links.html')
 
-url=$(echo $img_url | tr -d '"')
+download_url=$(echo $img_url)
 timestamp=$(date '+%Y/%m/%d %H:%M:%S')
 filename="$dir/$id.jpg"
 
-curl -o $filename $url
+curl -o $filename $download_url
 
 tmp_log=$(mktemp)
-jq --arg id "$id" --arg description "$desc" --arg timestamp "$timestamp" --arg url "$url" '. += [{id: $id, description: $description, timestamp: $timestamp, url: $url}]' $LOG_FILE >$tmp_log
+jq --arg id "$id" \
+	--arg description "$desc" \
+	--arg timestamp "$timestamp" \
+	--arg download_url "$download_url" \
+	--arg html_link "$html_link" \
+	'. += [{id: $id, description: $description, timestamp: $timestamp, download_url: $download_url, html_link: $html_link}]' $LOG_FILE >$tmp_log
 mv $tmp_log $LOG_FILE
 
 feh --bg-fill $filename
