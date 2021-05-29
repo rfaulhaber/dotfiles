fe418757ce70b9bef3126f27b865e11caa8f2072
Ryan Faulhaber <ryf@sent.as>
2021-05-29 11:50:42 -0400

    [DOOM] fixes bug where images in org files don't export

--- doom.d/config.el
+++ doom.d/config.el
@@ -256,3 +256,10 @@
 ;;     (:exec    . ("%c -i %s -o %s.svg" "cat %s.svg"))
 ;;     (:outputter . "browser"))
 ;;   :mode 'mermaid-mode)
+
+;; see: https://github.com/hlissner/doom-emacs/issues/3185
+
+(defadvice! self/+org-inline-image-data-fn (_protocol link _description)
+  :override #'+org-inline-image-data-fn
+  "Interpret LINK as base64-encoded image data. Ignore all errors."
+  (with-demoted-errors "%S" (base64-decode-string link)))
