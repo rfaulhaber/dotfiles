9903cb01c7ab10777c04c546e5a9e93f7f70b6e0
Ryan Faulhaber <ryf@sent.as>
2021-06-15 19:52:45 -0400

    removed code I didn't understand

--- flake.nix
+++ flake.nix
@@ -8,42 +8,24 @@
     darwin.url = "github:lnl7/nix-darwin";
   };
 
-  outputs = inputs@{ self, nixpkgs, darwin, ... }:
-    let
-      inherit (lib.my) mapModules mapModulesRec mapHosts;
-
-      mkPkgs = pkgs: extraOverlays:
-        import pkgs {
-          config.allowUnfree = true;
-          overlays = extraOverlays ++ (lib.attrValues self.overlays);
-        };
-      pkgs = mkPkgs nixpkgs [ self.overlay ];
-
-      lib = nixpkgs.lib.extend (self: super: {
-        my = import ./nix/lib {
-          inherit pkgs inputs;
-          lib = self;
-        };
-      });
-    in {
-      lib = lib.my;
-      nixosConfigurations = {
-        mir3 = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            inputs.home-manager.nixosModules.home-manager
-            (import ./nix)
-            {
-              desktop = {
-                bspwm.enable = true;
-                polybar.enable = true;
-              };
-            }
-          ];
-        };
-      };
-      darwinConfigurations.orange = darwin.lib.darwinSystem {
-        modules = [ ./nix/hosts/orange/configuration.nix ];
+  outputs = inputs@{ self, nixpkgs, darwin, ... }: {
+    nixosConfigurations = {
+      mir3 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          inputs.home-manager.nixosModules.home-manager
+          (import ./nix)
+          {
+            desktop = {
+              bspwm.enable = true;
+              polybar.enable = true;
+            };
+          }
+        ];
       };
     };
+    darwinConfigurations.orange = darwin.lib.darwinSystem {
+      modules = [ ./nix/hosts/orange/configuration.nix ];
+    };
+  };
 }
