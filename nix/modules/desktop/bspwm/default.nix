71e870d906d084c00b73b164e419ae8f45a17f0e
Ryan Faulhaber <ryf@sent.as>
2021-06-20 21:11:33 -0400

    [NIX] IMPORTANT NOTE

--- nix/modules/desktop/bspwm/default.nix
+++ nix/modules/desktop/bspwm/default.nix
@@ -28,7 +28,11 @@ in {
       # TODO modularize!
       videoDrivers = [ "nvidia" ];
     };
+    # NB: IN ORDER FOR ANY OF THIS TO WORK YOU NEED THIS SET!!
+    # I WASTED MOST OF A SUNDAY TRYING TO FIGURE THIS OUT!!!
+    # IT SURE WOULD HAVE BEEN GREAT TO KNOW THAT SOMEWHERE!!!!
     home-manager.users.${config.user.name}.xsession.enable = true;
+
     home.bspwm = {
       enable = true;
       monitors = {
