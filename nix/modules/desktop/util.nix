5a6f5358c0d7571ce07c7af1353d7fb4c9346d87
Ryan Faulhaber <ryf@sent.as>
2021-11-08 11:22:10 -0500

    [NIX] various changes
    - flake bump
    - change Zsh to be managed by home manager
    - make hardware scan not require pure
    - remove unused files

--- nix/modules/desktop/util.nix
+++ nix/modules/desktop/util.nix
@@ -21,7 +21,6 @@
       unzip
       wget
       zip
-      zoxide
     ];
   };
 }
--- nix/modules/programs/zsh/default.nix
+++ nix/modules/programs/zsh/default.nix
@@ -9,52 +9,64 @@ in {
   options.modules.programs.zsh = {
     enable = mkEnableOption false;
     setDefault = mkOption {
-      description = "Sets ZSH to be default shell for system user.";
+      description = "Sets Zsh to be default shell for system user.";
       default = false;
       type = types.bool;
     };
     theme = mkOption {
-      description = "ZSH theme from oh-my-zsh.";
+      description = "Zsh theme from oh-my-zsh.";
       default = "agnoster";
       type = types.str;
+    };
+    # TODO make shell agnostic
+    useZoxide = mkOption {
+      description = "Use Zoxide with Zsh.";
+      # I use Zoxide by default
+      default = true;
+      type = types.bool;
     };
   };
   config = mkIf cfg.enable {
     environment = {
       systemPackages = with pkgs; [
         nix-zsh-completions
-        oh-my-zsh
         xclip # required for pbcopy and pbpaste
-        zsh
-        zsh-autosuggestions
-        zsh-completions
       ];
 
       # sometimes zsh from nixpkgs doesn't respect highlightStyle value
       variables = { ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE = "fg=${colors.grey}"; };
     };
 
-    programs.zsh = {
-      enable = true;
-      ohMyZsh = {
+    home.programs = {
+      zsh = {
         enable = true;
-        plugins = [ "git" "colored-man-pages" ];
-        theme = cfg.theme;
+        enableAutosuggestions = true;
+        enableCompletion = true;
+        enableSyntaxHighlighting = true;
+
+        oh-my-zsh = {
+          enable = true;
+          plugins = [ "git" "colored-man-pages" ];
+          theme = cfg.theme;
+        };
+
+        shellAliases = {
+          pbcopy = "xclip -selection clipboard";
+          pbpaste = "xclip -selection clipboard -o";
+          ls = "exa";
+          l = "exa -lah";
+          ll = "exa -lh";
+          ec = "emacsclient";
+          eo = "emacsclient -n"; # "emacs open"
+        };
       };
-      autosuggestions.enable = true;
-      autosuggestions.highlightStyle = "fg=${colors.grey}";
-      syntaxHighlighting.enable = true;
-      shellAliases = {
-        pbcopy = "xclip -selection clipboard";
-        pbpaste = "xclip -selection clipboard -o";
-        ls = "exa";
-        l = "exa -lah";
-        ll = "exa -lh";
-        ec = "emacsclient";
-        eo = "emacsclient -n"; # "emacs open"
-      };
+
+      zoxide.enable = (mkIf cfg.useZoxide) true;
     };
 
     user.shell = mkIf cfg.setDefault pkgs.zsh;
+
+    # required by home-manager zsh
+    environment.pathsToLink = [ "/share/zsh" ];
   };
 }
