b82698c98c848f385547e71d161cc04797c8d9df
Ryan Faulhaber <ryf@sent.as>
2021-12-04 16:39:06 -0500

    [NIX] various config changes, stateVersion bump

--- nix/hosts/nil/configuration.nix
+++ nix/hosts/nil/configuration.nix
@@ -41,13 +41,23 @@
   boot.tmpOnTmpfs = true;
   boot.cleanTmpDir = true;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "21.11"; # Did you read the comment?
+  nix = {
+    autoOptimiseStore = true;
+    package = pkgs.nixFlakes;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+  };
+
+  system = {
+    autoUpgrade = { enable = true; };
+    stateVersion = "21.11";
+  };
 
 }
 
