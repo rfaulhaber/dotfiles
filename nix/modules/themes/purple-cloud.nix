c8aa7eec21d228c5abdd686fc0b6017cedf78e7e
Ryan Faulhaber <ryf@sent.as>
2022-02-16 18:46:49 -0500

    [NIX] purple cloud theme. wip

---
+++ nix/modules/themes/purple-cloud.nix
@@ -0,0 +1,31 @@
+# purple cloud theme
+
+{
+  bg = "#10061c";
+  fg = "#f5e8ff";
+
+  black = "#222827";
+  blue = "#654a96";
+  cyan = "#a9d1df";
+  green = "#9c75dd";
+  magenta = "#5c4e67";
+  red = "#d5a8e3";
+  white = "#e6ebe5";
+  yellow = "#9898ae";
+
+  bright-black = "#5d6f74";
+  bright-blue = "#5786bc";
+  bright-cyan = "#849da2";
+  bright-green = "#63b0b0";
+  bright-magenta = "#3f3442";
+  bright-red = "#cd749c";
+  bright-white = "#d9d6cf";
+  bright-yellow = "#c0c0dd";
+
+  # extra colors beyond the base 16 we need
+  bg-alt = "#10061c";
+  dark-cyan = "#008B94";
+  fg-alt = "#f5e8ff";
+  grey = "#41505E";
+  orange = "#D98E48";
+}
