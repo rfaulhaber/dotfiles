dcc65ad074fa02f184588c85a2112878686f0d9d
Ryan Faulhaber <ryf@sent.as>
2022-06-05 17:26:01 -0400

    [DOOM] set firefox as default browser on hyperion

--- doom.d/hosts/hyperion.el
+++ doom.d/hosts/hyperion.el
@@ -6,3 +6,6 @@
 ;; on hyperion I have racket installed globally. as of writing this it is the only
 ;; scheme I have installed globally!
 (setq geiser-active-implementations '(racket))
+
+(setq browse-url-browser-function 'browse-url-firefox
+      browse-url-firefox-program "firefox-devedition")
