5630025a6c96ac80a186d297b6d2f3a8485a9aa9
Ryan Faulhaber <ryf@sent.as>
2022-08-13 23:41:59 -0400

    [NIX] important note

--- nix/modules/services/syncthing/default.nix
+++ nix/modules/services/syncthing/default.nix
@@ -1,5 +1,8 @@
 { config, lib, pkgs, ... }:
 
+# NOTE: might be worth looking into some of the advanced Nix options:
+# https://search.nixos.org/options?channel=unstable&from=0&size=50&sort=relevance&type=packages&query=syncthing
+
 with lib;
 
 let cfg = config.modules.services.syncthing;
