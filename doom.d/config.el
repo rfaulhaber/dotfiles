633e2e009abc65b040982d5f92dfd6fb1e277479
Ryan Faulhaber <ryf@sent.as>
2022-09-01 23:59:26 -0400

    [CONFIG] add encryption shortcuts

--- doom.d/config.el
+++ doom.d/config.el
@@ -100,6 +100,9 @@
       :n "TAB"        #'man-follow)
 (map! :leader "TAB c" #'+workspace/cycle)
 (map! :leader "n j t" #'org-journal-open-current-journal-file)
+(map! :mode org-journal-mode
+      :leader "n j e" #'self/org-journal-make-entry-encrypted
+      :leader "n j d" #'self/org-journal-make-entry-unencrypted)
 (map! :leader "f o"   #'self/find-org-file)
 (map! :leader "f O"   #'self/visit-common-directories)
 (map! :leader "d"     #'dired)
