63efd4adeadc80f432d76ef4bc508e4c16384bc2
Ryan Faulhaber <ryf@sent.as>
2022-11-28 09:34:36 -0500

    [NIX] update rust project template

--- nix/templates/rust/flake.nix
+++ nix/templates/rust/flake.nix
@@ -1,15 +1,33 @@
+# barebones Rust project template with Nix flakes
 {
-  description = "Rust project flake template";
+  description = "Nix Rust flake template";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        devShell = pkgs.mkShell {
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        projectName = "project";
+      in rec {
+        packages.${projectName} = pkgs.rustPlatform.buildRustPackage {
+          pname = projectName;
+          version = "0.1.0";
+          src = ./.;
+          # NOTE: make sure Cargo.lock is not in .gitignore
+          cargoLock.lockFile = ./Cargo.lock;
+        };
+
+        packages.default = self.packages.${system}.${projectName};
+
+        apps.${projectName} =
+          flake-utils.lib.mkApp { drv = packages.${projectName}; };
+
+        apps.default = self.apps.${system}.${projectName};
+
+        devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             cargo
             rustc
