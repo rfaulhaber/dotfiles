437cce16d2a6df91f1bc9a0ea496bc84e00bbb72
Ryan Faulhaber <ryf@sent.as>
2023-03-10 15:26:12 -0500

    [NIX] add emacs lisp template

---
+++ nix/templates/emacs-lisp/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Emacs Lisp template";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      projectName = "package";
+    in rec {
+      defaultPackage = pkgs.stdenv.mkDerivation {};
+      # apps.${projectName} = flake-utils.lib.mkApp { drv = pkgs.${projectName}; };
+      # defaultApp = apps.${projectName}};
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          emacs
+          nodejs-19_x
+          nodePackages_latest.eask
+        ];
+      };
+    });
+}
