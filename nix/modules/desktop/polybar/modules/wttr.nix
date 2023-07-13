ee5c6cf65a79558ee6ac5f7654258f798810ddb7
Ryan Faulhaber <ryf@sent.as>
2023-07-13 14:43:29 -0400

    [NIX] make wttr script have longer interval

--- nix/modules/desktop/polybar/modules/wttr.nix
+++ nix/modules/desktop/polybar/modules/wttr.nix
@@ -8,7 +8,7 @@
     latitude = toString config.userInfo.location.latitude;
     longitude = toString config.userInfo.location.longitude;
   in "${pkgs.nushell}/bin/nu ${config.dotfiles.binDir}/polybar/wttr.nu --latitude ${latitude} --longitude ${longitude}";
-  interval = "900";
+  interval = "3600";
   # TODO avoid hard code, refactor
   click-left = "${pkgs.firefox-devedition-bin}/bin/firefox-devedition https://openweathermap.org/city/5150529 >/dev/null 2>&1 &";
 }
