88dbbf7db4a465e723da0b49674bd27c4faef6ae
Ryan Faulhaber <ryf@sent.as>
2023-11-25 10:03:37 -0500

    [NIX] bump hash for awesome

--- nix/overlays/awesome.nix
+++ nix/overlays/awesome.nix
@@ -7,7 +7,7 @@
         owner = "awesomeWM";
         repo = "awesome";
         rev = "375d9d723550023f75ff0066122aba99fdbb2a93";
-        sha256 = "sha256-p2cpQhKj/mhd8904MnjGbXf70cT/coG4XdIB5P/hvW4=";
+        sha256 = "sha256-9cIQvuXUPu8io2Qs3Q8n2WkF9OstdaGUt/+0FMrRkXk=";
         fetchSubmodules = false;
       };
       patches = [];
