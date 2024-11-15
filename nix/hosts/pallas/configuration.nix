b6213e78d90fa0e0eee3696406ec1b80ea00a6ed
Ryan Faulhaber <ryf@sent.as>
2024-11-15 15:57:29 -0500

    [NIX] more ssh stuff

--- nix/hosts/pallas/configuration.nix
+++ nix/hosts/pallas/configuration.nix
@@ -39,6 +39,7 @@
           keys = [
             "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHlCcs8h2PrT3GcOVs6K0IGozqV8yuR945ZDr8eYhqfj ryan@hyperion"
             "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID9EVRAxaCrK68NSCoiNjjQLqu4k13Z45tCBb0jGAtC/ ryan@eos"
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPhN+t0aI3pQhZsFPoRn8dWe7YvDn3ehWOUmwvqbQyZP ryan@1p"
           ];
           port = 12981;
         };
