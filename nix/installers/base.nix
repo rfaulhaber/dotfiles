85e326a373414be83dd851cd00c7460a7944c163
Ryan Faulhaber <ryf@sent.as>
2024-11-15 15:36:29 -0500

    [NIX] more ssh key replacement

--- nix/installers/base.nix
+++ nix/installers/base.nix
@@ -11,7 +11,7 @@
 
   users.extraUsers.nixos = {
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAZQ6dhGnjyJ+SBMeN5IRHcpV6ERR+a/WPmvD7o2TM90 ryan@hyperion"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHlCcs8h2PrT3GcOVs6K0IGozqV8yuR945ZDr8eYhqfj ryan@hyperion"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID9EVRAxaCrK68NSCoiNjjQLqu4k13Z45tCBb0jGAtC/ ryan@eos"
     ];
 
