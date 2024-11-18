7753326f28f9a8bf14e5627bddaeeb155c2f538e
Ryan Faulhaber <ryf@sent.as>
2024-11-18 16:39:39 -0500

    [NIX] wezterm notes

--- nix/modules/programs/wezterm/default.nix
+++ nix/modules/programs/wezterm/default.nix
@@ -15,12 +15,16 @@ in {
   config = mkIf cfg.enable {
     user.packages = [inputs.wezterm.packages.${pkgs.system}.default];
 
+    # NOTE: using home-manager for wezterm creates a single wezterm.lua file
+    # this is undesirable because of my wezterm setup. instead we just link my
+    # wezterm config here
     home.file.wezterm_config = {
       source = "${config.dotfiles.configDir}/wezterm";
       target = "${config.user.home}/.config/wezterm";
       recursive = true;
     };
 
+    # allows us to use the cached version of wezterm for the wezterm input
     nix.settings = {
       substituters = ["https://wezterm.cachix.org"];
       trusted-public-keys = ["wezterm.cachix.org-1:kAbhjYUC9qvblTE+s7S+kl5XM1zVa4skO+E/1IDWdH0="];
