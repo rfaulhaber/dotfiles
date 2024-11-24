52aa57d3c237bbb2196fb9306c46c1dc483a27f2
Ryan Faulhaber <ryf@sent.as>
2024-11-24 16:28:46 -0500

    [NIX] flake bump, yubikey changes

--- nix/modules/services/yubikey/default.nix
+++ nix/modules/services/yubikey/default.nix
@@ -27,8 +27,14 @@ in {
         authfile = pkgs.writeText "u2f-mappings" (lib.concatStrings [
           config.user.name
           ":mEWrvBLxquMeVyb7wN0MQ5+qQobyMQDPm7EbSasf4O7obEgcngY40mq+Tq9gc/IIbIiLkA9p++7vGzb8k1mkpw==,T45SuFOGn+U+wiQ39BQ/xTG3oBHf1JjzEgckVt1mEItFjdDuPf3dHhqM71cylSfueOPh1Ox8x36mgfz0i+hV1g==,es256,+presence"
+          ":dZHv9JRgYWuzXXTpFsayd23nkGlOF4WpiI7PomATSuki75VAA85A9boB6IFyCAXurwoCcPKIDbIHt5gHdlaTcw==,Jz34hI0hqZW4JY/NL0b5z9tn55tILOkUFyR3Gn4Xjp9ynqlFfu7yYmHpImUIgpPNdm6O4X3922b3Aezb/n18AA==,es256,+presence"
         ]);
       };
     };
+
+    user.packages = with pkgs; [
+      yubikey-manager
+      yubikey-personalization
+    ];
   };
 }
