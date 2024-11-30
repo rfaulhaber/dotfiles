297c044f2e65922d17a7a1a439902185a6b6d971
Ryan Faulhaber <ryf@sent.as>
2024-11-30 14:02:06 -0500

    [NIX] add disko flake

--- flake.nix
+++ flake.nix
@@ -39,6 +39,10 @@
       url = "github:wez/wezterm?dir=nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    disko = {
+      url = "github:nix-community/disko/latest";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs @ {
