581c17810ad9ba89f2f63ec1a6912b97c4a2e58f
Ryan Faulhaber <ryf@sent.as>
2024-12-15 15:39:31 -0500

    [DOOM] fix path/exec-path issue

--- doom.d/hosts/eos.el
+++ doom.d/hosts/eos.el
@@ -1,3 +1,22 @@
 ;;; ../Projects/dotfiles/doom.d/hosts/eos.el -*- lexical-binding: t; -*-
 
 (set-frame-size (selected-frame) 145 46)
+
+;; not sure why the PATH isn't loading correctly, so I've opted to manually fix
+;; it here
+(let ((path-elements '("/Users/ryan/Library/Application Support/carapace/bin"
+                       "/Users/ryan/.nix-profile/bin"
+                       "/opt/homebrew/bin"
+                       "/Users/ryan/.emacs.d/bin"
+                       "/Users/ryan/.config/emacs/bin"
+                       "/usr/bin"
+                       "/bin"
+                       "/usr/sbin"
+                       "/sbin"
+                       "/usr/local/bin"
+                       "/nix/var/nix/profiles/default/bin")))
+  (cl-loop for el in path-elements
+           do
+           (add-to-list 'exec-path el))
+
+  (setenv "PATH" (string-join path-elements ":")))
