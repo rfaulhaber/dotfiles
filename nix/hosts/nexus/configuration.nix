2e0e4c6554741408d9fa1dd92217e6abb729e6ab
Ryan Faulhaber <ryf@sent.as>
2024-12-23 17:06:18 -0500

    [NIX] add nexus

---
+++ nix/hosts/nexus/configuration.nix
@@ -0,0 +1,61 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ../../modules
+  ];
+
+  modules = {
+    programs = {
+      nushell = {
+        enable = true;
+        setDefault = true;
+        carapace.enable = true;
+      };
+      neovim.enable = true;
+      git.enable = true;
+    };
+    services = {
+      ssh = {
+        enable = true;
+        server = {
+          enable = true;
+          keys = [
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHlCcs8h2PrT3GcOVs6K0IGozqV8yuR945ZDr8eYhqfj ryan@hyperion"
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID9EVRAxaCrK68NSCoiNjjQLqu4k13Z45tCBb0jGAtC/ ryan@eos"
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPhN+t0aI3pQhZsFPoRn8dWe7YvDn3ehWOUmwvqbQyZP ryan@1p"
+          ];
+          port = 14557;
+        };
+      };
+
+      zerotier = {
+        enable = true;
+        networks = ["b6079f73c6986bc2"];
+      };
+    };
+
+    themes.active = "moonlight";
+  };
+
+  boot = {
+    loader = {
+      grub.enable = false;
+      generic-extlinux-compatible.enable = true;
+    };
+  };
+
+  networking = {
+    hostname = "nexus";
+    useDHCP = true;
+    interfaces.end0.useDHCP = true;
+  };
+}
