c9879b312d35e34639cd1c59f860381d234de68b
Ryan Faulhaber <ryf@sent.as>
2025-01-21 15:35:42 -0500

    [WEZTERM] some macOS keyboard updates

--- config/wezterm/darwin.lua
+++ config/wezterm/darwin.lua
@@ -1,3 +1,22 @@
+local wezterm = require 'wezterm'
+
 return {
-  default_prog = { "/opt/homebrew/bin/nu" }
+  default_prog = { "/opt/homebrew/bin/nu" },
+  keys = {
+    {
+        key = "p",
+        mods = "CMD",
+        action = wezterm.action.ActivateCommandPalette,
+    },
+    {
+      key = "v",
+      mods = "CMD|ALT",
+      action = wezterm.action.SplitVertical {domain="CurrentPaneDomain"},
+    },
+    {
+      key = "h",
+      mods = "CMD|ALT",
+      action = wezterm.action.SplitHorizontal {domain="CurrentPaneDomain"}
+    },
+  }
 }
