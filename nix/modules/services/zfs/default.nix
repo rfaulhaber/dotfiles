0da73b90d1fb4a416133bbcbdcce61478181496c
Ryan Faulhaber <ryf@sent.as>
2025-06-09 17:12:20 -0400

    [NIX] trying security.pam.zfs

--- nix/modules/services/zfs/default.nix
+++ nix/modules/services/zfs/default.nix
@@ -21,6 +21,14 @@ in {
         };
       });
     };
+    # NOTE in order for this to work properly, see the following:
+    # - https://wiki.archlinux.org/title/ZFS (section 6.1.2)
+    # - https://www.reddit.com/r/NixOS/comments/tzksw4/comment/i4dw7f8
+    encryptedHome = mkOption {
+      description = "Home dataset to be decrypted on boot.";
+      type = types.nullOr types.str;
+      default = null;
+    };
   };
 
   config = mkIf cfg.enable {
@@ -30,10 +38,20 @@ in {
         message = "ZFS is marked as broken in this kernel package. Please address.";
       }
     ];
+
     services.zfs = {
       autoScrub.enable = true;
       trim.enable = true;
       autoSnapshot.enable = true;
     };
+
+    # TODO throw in some assertions to check this?
+    security.pam.zfs = mkIf (cfg.encryptedHome != null) {
+      enable = true;
+      homes = cfg.encryptedHome;
+    };
+
+    # TODO need way to override for multiple encrypted datasets
+    boot.zfs.requestEncryptionCredentials = cfg.encryptedHome == null;
   };
 }
