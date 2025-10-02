4466ca7343a6ef03fdf00966fc997689676f6399
Ryan Faulhaber <ryf@sent.as>
2025-10-02 14:00:42 -0400

    [WEZTERM] switch to hardware rendering on hyperion

--- config/wezterm/hyperion.lua
+++ config/wezterm/hyperion.lua
@@ -2,7 +2,7 @@ local os = require "os";
 
 return {
 	font_size = 20,
-	front_end = "WebGpu",
+	front_end = "OpenGL",
     -- wezterm uses its own SSH agent. we don't want this
     default_ssh_auth_sock = os.getenv 'SSH_AUTH_SOCK',
 }
