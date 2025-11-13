20726d9634ba6e49c0b996847a4c57e1b5d02801
Ryan Faulhaber <ryf@sent.as>
2025-11-13 15:22:02 -0500

    [NIX] container tinkering

--- nix/modules/linux/services/containers/mullvad.nix
+++ nix/modules/linux/services/containers/mullvad.nix
@@ -31,100 +31,44 @@ in {
   };
 
   config = mkIf cfg.enable {
-    # Configure the container
-
-    boot.kernel.sysctl = {
-      "net.ipv4.ip_forward" = 1;
-      "net.ipv6.conf.all.forwarding" = 1;
-    };
-
-    systemd.network = {
-      enable = true;
-
-      netdevs."10-br-mullvad" = {
-        netdevConfig = {
-          Kind = "bridge";
-          Name = "br-mullvad";
-        };
-      };
-
-      networks."10-br-mullvad" = {
-        matchConfig.Name = "br-mullvad";
-        address = ["10.23.0.1/24"];
-        networkConfig = {
-          IPv4Forwarding = true;
-          IPMasquerade = "ipv4";
-          # Don't wait for this interface to be "online"
-          ConfigureWithoutCarrier = true;
-        };
-        # Ignore this interface for wait-online
-        linkConfig.RequiredForOnline = "no";
+    networking = let
+      interfaceName = "br0";
+    in {
+      bridges.${interfaceName}.interfaces = [
+        "enp5s0"
+      ];
+      interfaces.${interfaceName} = {
+        useDHCP = true;
       };
     };
 
     # TODO see https://wiki.nixos.org/wiki/NixOS_Containers#Networking
-    containers.mullvad-vpn = {
+    containers.mullvad = {
       # We need a private network to isolate it from the host
       privateNetwork = true;
       # Define the IPs for the virtual ethernet (veth) pair
       # Host side: 10.23.0.1
       # Container side (host0 interface): 10.23.0.2
-      hostAddress = "10.23.0.1";
-      localAddress = "10.23.0.2";
+      # hostAddress = "10.23.0.1";
+      localAddress = "192.168.0.225";
+      hostBridge = "br0";
       bindMounts.${cfg.privateKeyFile}.isReadOnly = true;
 
       # This is the configuration for the OS *inside* the container
       config = {...}: {
         system.stateVersion = "24.05"; # Or your host's version
         boot.isContainer = true;
-
         networking = {
+          hostName = "container-mullvad";
+          # Use systemd-resolved inside the container
+          # Workaround for bug https://github.com/NixOS/nixpkgs/issues/162686
           useHostResolvConf = lib.mkForce false;
-          firewall.enable = false;
-          nameservers = ["10.64.0.1"]; # Mullvad DNS
-
-          wg-quick.interfaces = {
-            wg0 = {
-              address = cfg.mullvadConfig.addresses;
-              # use dnscrypt, or proxy dns as described above
-              dns = ["10.64.0.1"];
-              privateKeyFile = cfg.privateKeyFile;
-              peers = [
-                {
-                  # bt wg conf
-                  publicKey = cfg.mullvadConfig.publicKey;
-                  allowedIPs = [
-                    "0.0.0.0/0"
-                  ];
-                  endpoint = "${cfg.mullvadConfig.endpoint}:51820";
-                  persistentKeepalive = 25;
-                }
-              ];
-            };
-          };
+          useDHCP = false;
+          interfaces.eth0.useDHCP = true;
         };
 
-        systemd.services."wg-quick-wg-mullvad" = {
-          wantedBy = ["multi-user.target"];
-          after = ["network.target"];
-        };
-
+        services.resolved.enable = true;
         environment.systemPackages = [pkgs.wireguard-tools];
-      };
-    };
-
-    systemd.services."container@mullvad-vpn" = {
-      after = ["systemd-networkd.service"];
-      wants = ["systemd-networkd.service"];
-    };
-
-    # Tell systemd-networkd-wait-online to ignore the bridge
-    systemd.services.systemd-networkd-wait-online = {
-      serviceConfig = {
-        ExecStart = [
-          "" # Clear the default
-          "${config.systemd.package}/lib/systemd/systemd-networkd-wait-online --ignore=br-mullvad"
-        ];
       };
     };
   };
