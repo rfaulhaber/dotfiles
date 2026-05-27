d6296c8fffae0ceba4bff3620887ae123ed0e35e
Ryan Faulhaber <ryf@sent.as>
2026-05-27 13:59:58 -0400

    [NIX] update niri config for multiple monitors, delete old kdl file

--- nix/modules/linux/services/desktop/environment/niri/binds.nix
+++ nix/modules/linux/services/desktop/environment/niri/binds.nix
@@ -62,23 +62,23 @@ in {
   "Mod+Ctrl+End" = action "move-column-to-last";
 
   # Monitor focus/move
-  "Mod+Shift+Left" = action "focus-monitor-left";
-  "Mod+Shift+Down" = action "focus-monitor-down";
-  "Mod+Shift+Up" = action "focus-monitor-up";
-  "Mod+Shift+Right" = action "focus-monitor-right";
-  "Mod+Shift+H" = action "focus-monitor-left";
-  "Mod+Shift+J" = action "focus-monitor-down";
-  "Mod+Shift+K" = action "focus-monitor-up";
-  "Mod+Shift+L" = action "focus-monitor-right";
+  "Mod+Alt+Left" = action "focus-monitor-left";
+  "Mod+Alt+Down" = action "focus-monitor-down";
+  "Mod+Alt+Up" = action "focus-monitor-up";
+  "Mod+Alt+Right" = action "focus-monitor-right";
+  "Mod+Alt+H" = action "focus-monitor-left";
+  "Mod+Alt+J" = action "focus-monitor-down";
+  "Mod+Alt+K" = action "focus-monitor-up";
+  "Mod+Alt+L" = action "focus-monitor-right";
 
-  "Mod+Shift+Ctrl+Left" = action "move-column-to-monitor-left";
-  "Mod+Shift+Ctrl+Down" = action "move-column-to-monitor-down";
-  "Mod+Shift+Ctrl+Up" = action "move-column-to-monitor-up";
-  "Mod+Shift+Ctrl+Right" = action "move-column-to-monitor-right";
-  "Mod+Shift+Ctrl+H" = action "move-column-to-monitor-left";
-  "Mod+Shift+Ctrl+J" = action "move-column-to-monitor-down";
-  "Mod+Shift+Ctrl+K" = action "move-column-to-monitor-up";
-  "Mod+Shift+Ctrl+L" = action "move-column-to-monitor-right";
+  "Mod+Alt+Ctrl+Left" = action "move-column-to-monitor-left";
+  "Mod+Alt+Ctrl+Down" = action "move-column-to-monitor-down";
+  "Mod+Alt+Ctrl+Up" = action "move-column-to-monitor-up";
+  "Mod+Alt+Ctrl+Right" = action "move-column-to-monitor-right";
+  "Mod+Alt+Ctrl+H" = action "move-column-to-monitor-left";
+  "Mod+Alt+Ctrl+J" = action "move-column-to-monitor-down";
+  "Mod+Alt+Ctrl+K" = action "move-column-to-monitor-up";
+  "Mod+Alt+Ctrl+L" = action "move-column-to-monitor-right";
 
   # Workspace navigation
   "Mod+Page_Down" = action "focus-workspace-down";
