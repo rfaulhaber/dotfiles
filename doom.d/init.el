8bc51ac5102babd77d631ead57894ef1d2be0099
Ryan Faulhaber <ryf@sent.as>
2026-07-22 11:32:55 -0400

    clean up init.el

--- doom.d/init.el
+++ doom.d/init.el
@@ -23,7 +23,7 @@
 
        :ui
        doom              ; what makes DOOM look the way it does
-       doom-dashboard    ; a nifty splash screen for Emacs
+       dashboard         ; a nifty splash screen for Emacs
        doom-quit         ; DOOM quit-message prompts when you quit Emacs
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        indent-guides     ; highlighted indent columns
@@ -36,63 +36,57 @@
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        window-select     ; visually switch windows
        workspaces        ; tab emulation, persistence & separate workspaces
-       zen               ; distraction-free coding or writing
        (emoji +unicode)
 
        :editor
-       (evil +everywhere); come to the dark side, we have cookies
-       file-templates    ; auto-snippets for empty files
-       fold              ; (nigh) universal code folding
-       (format +onsave +lsp)  ; automated prettiness
-       rotate-text       ; cycle region at point between text candidates
-       snippets          ; my elves. They type so I don't have to
+       (evil +everywhere)    ; come to the dark side, we have cookies
+       file-templates        ; auto-snippets for empty files
+       fold                  ; (nigh) universal code folding
+       (format +onsave +lsp) ; automated prettiness
+       rotate-text           ; cycle region at point between text candidates
+       snippets              ; my elves. They type so I don't have to
 
        :emacs
-       (dired +dirvish +icons)            ; making dired pretty [functional]
-       electric          ; smarter, keyword-based electric-indent
-       ibuffer         ; interactive buffer management
-       undo              ; persistent, smarter undo for your inevitable mistakes
-       vc                ; version-control and Emacs, sitting in a tree
+       (dired +dirvish +icons) ; making dired pretty [functional]
+       electric                ; smarter, keyword-based electric-indent
+       ibuffer                 ; interactive buffer management
+       undo                    ; persistent, smarter undo for your inevitable mistakes
+       vc                      ; version-control and Emacs, sitting in a tree
 
        :term
        (ghostel +everywhere)
 
        :checkers
        (syntax +flymake +icons)              ; tasing you for every semicolon you forget
-       (spell +flyspell +aspell +everywhere)            ; tasing you for misspelling mispelling
-       grammar           ; tasing grammar mistake every you make
+       (spell +flyspell +aspell +everywhere) ; tasing you for misspelling mispelling
+       grammar                               ; tasing grammar mistake every you make
 
        :tools
-       debugger          ; FIXME stepping through code, to help you add bugs
+       debugger                               ; FIXME stepping through code, to help you add bugs
        direnv
        docker
-       editorconfig      ; let someone else argue about tabs vs spaces
-       (eval +overlay)     ; run code, run (also, repls)
-       (lookup +dictionary +offline +docsets)              ; navigate your code and its documentation
+       editorconfig                           ; let someone else argue about tabs vs spaces
+       (eval +overlay)                        ; run code, run (also, repls)
+       (lookup +dictionary +offline +docsets) ; navigate your code and its documentation
        (lsp +eglot +peek)
-       magit             ; a git porcelain for Emacs
-       pdf               ; pdf enhancements
+       magit                                  ; a git porcelain for Emacs
+       pdf                                    ; pdf enhancements
 
        :lang
-                                        ;common-lisp       ; if you've seen one lisp, you've seen them all
-       data              ; config/data formats
-       (elixir +lsp)           ; erlang done right
-       emacs-lisp        ; drown in parentheses
-       (json +lsp)              ; At least it ain't XML
-       (javascript +lsp)          ; all(hope(abandon(ye(who(enter(here))))))
-       latex             ; writing papers in Emacs has never been so fun
-       markdown            ; writing docs for people to ignore
-       (nix +lsp)              ; I hereby declare "nix geht mehr!"
-       (org +hugo +journal +pandoc +present +roam2 +noter +dragndrop +pretty +pomodoro)               ; organize your plain life in plain text
-       (python +lsp +pyright)           ; beautiful is better than ugly
-       (rust +lsp)               ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
-                                        ;scheme            ; a fully conniving family of lisps
-       (sh +lsp)         ; she sells {ba,z,fi}sh shells on the C xor
-       (web +lsp)               ; the tubes
-       yaml              ; JSON, but readable
-
-       :email
-       ;; (mu4e +gmail)
+       data                                                                             ; config/data formats
+       (elixir +lsp)                                                                    ; erlang done right
+       emacs-lisp                                                                       ; drown in parentheses
+       (json +lsp)                                                                      ; At least it ain't XML
+       (javascript +lsp)                                                                ; all(hope(abandon(ye(who(enter(here))))))
+       latex                                                                            ; writing papers in Emacs has never been so fun
+       markdown                                                                         ; writing docs for people to ignore
+       (nix +lsp)                                                                       ; I hereby declare "nix geht mehr!"
+       (org +hugo +journal +pandoc +present +roam2 +noter +dragndrop +pretty +pomodoro) ; organize your plain life in plain text
+       (python +lsp +pyright)                                                           ; beautiful is better than ugly
+       (rust +lsp)                                                                      ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       (sh +lsp)                                                                        ; she sells {ba,z,fi}sh shells on the C xor
+       (web +lsp)                                                                       ; the tubes
+       yaml                                                                             ; JSON, but readable
 
        :app
        everywhere
@@ -101,5 +95,4 @@
        (:if IS-MAC macos)             ; MacOS-specific commands
 
        :config
-       ;;literate
        (default +bindings +smartparens))
