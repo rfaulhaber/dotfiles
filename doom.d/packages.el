0e772d9cdff0143557d5604a6820caf241dc1a98
Ryan Faulhaber <ryf@sent.as>
2026-08-21 11:11:31 -0400

    flake bump

--- doom.d/packages.el
+++ doom.d/packages.el
@@ -92,7 +92,7 @@
 ;; :around advice on ghostel--redraw must match the native fn's arity
 ;; (v0.44.0 added FORCE-SYNC; a stale advice signals
 ;; wrong-number-of-arguments on every redraw and blanks the terminal).
-(let ((ghostel-pin "57c320310f5eec0c5826d613b2806d7237606469"))
+(let ((ghostel-pin "02d0e3743dbe1a8c607adcfdc526367d798f4c23"))
   (package! ghostel :pin ghostel-pin)
   (package! evil-ghostel :pin ghostel-pin))
 
