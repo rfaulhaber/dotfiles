6862f2092b423844c3f4aadbc89c5967e061d259
Ryan Faulhaber <ryf@sent.as>
2026-09-18 10:50:15 -0400

    deprecate sops overlay

--- nix/modules/programs/sops/default.nix
+++ nix/modules/programs/sops/default.nix
@@ -10,7 +10,6 @@
 }:
 with lib; let
   cfg = config.modules.programs.sops;
-  goBuilderAlias = import ../../../overlays/sops_install_secrets_20260917.nix inputs.sops-nix;
 in {
   imports =
     lib.optionals isLinux [
@@ -45,9 +44,6 @@ in {
         message = "$host/secrets.yaml must exist";
       }
     ];
-
-    nixpkgs.overlays = [goBuilderAlias.overlay];
-    warnings = [goBuilderAlias.warning];
 
     sops = {
       inherit (cfg) secrets;
